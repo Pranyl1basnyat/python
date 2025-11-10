d65e4c51d2eff3d54341d886bb3db4cb6035bfa4
Pranyl1basnyat <pranylbasnyat@gmail.com>
2025-11-10 21:14:59 +0545

    This is  the code

--- filehamdlig/basic.py
+++ filehamdlig/basic.py
@@ -1,10 +1,12 @@
-open("student.txt","w")
-n=int(input("How many records do you wamt to add "))
-for i in range(1,n):
-    name=str(input("Enter the name of the student "))
-    age=int(input("Enter the age of the student "))
-    grade=str(input("Enter the grade of the student "))
-    address=str(input("Enter the address  of the student "))
-    address=str(input("Enter the address  of the student "))
-    math=int(input("Enter the marks of  the maht "))
+with open("student.txt","a") as f:
+    n=int(input("How many records do you wamt to add "))
+    for i in range(1,n):
+         name=str(input("Enter the name of the student "))
+         age=int(input("Enter the age of the student "))
+         grade=str(input("Enter the grade of the student "))
+         address=str(input("Enter the address  of the student "))
+         math=int(input("Enter the marks of  the maht "))
+         f.write(f"{name},{age},{grade},{address},{math}\n")
+    f.close()
+    
     
