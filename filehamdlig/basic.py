68f08d5fab2354a186821044562a048218b3b198
Pranyl1basnyat <pranylbasnyat@gmail.com>
2025-11-10 20:54:58 +0545

    file handling writting operation

---
+++ filehamdlig/basic.py
@@ -0,0 +1,10 @@
+open("student.txt","w")
+n=int(input("How many records do you wamt to add "))
+for i in range(1,n):
+    name=str(input("Enter the name of the student "))
+    age=int(input("Enter the age of the student "))
+    grade=str(input("Enter the grade of the student "))
+    address=str(input("Enter the address  of the student "))
+    address=str(input("Enter the address  of the student "))
+    math=int(input("Enter the marks of  the maht "))
+    
